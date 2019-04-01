920054acb6aedbcd7d49304e96806a55726c4774
Tami Churns <tchurns@paperlesspost.com>
2019-04-01 23:56:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ window.addEventListener("load", () => {
     const input = document.getElementById('password-length')
     const howManyCharacters = input.value
     // create a loop that replaces the number inputted, and returns a random letter each time:
+    let startingString = ''
     for (let i = 0; i < howManyCharacters; i++){
       //what do you want the loop to do? pick a random letter in this case
      const randomIndex = Math.floor(Math.random() * 26)
-     console.log(lowerCaseLetters[randomIndex])
+     startingString += lowerCaseLetters[randomIndex]
     }
   }
   
