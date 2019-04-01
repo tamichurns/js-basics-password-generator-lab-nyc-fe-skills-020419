d092d8308f115abd6f45633bc3d7e7716873228b
Tami Churns <tchurns@paperlesspost.com>
2019-04-01 23:38:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ window.addEventListener("load", () => {
   //start your code here
   
   const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
-  
   const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-  
   const numbers = "0123456789"
-  
   const specialCharacter = "!#$%&()*+,-./:;<=>?@[\]^_{|}~"
   
-  document.addEventListener('submit', onSubmit)
   
   function onSubmit(event){
     event.preventDefault()
     const input = document.getElementById('input')
     console.log(input.value)
   }
+  
+  document.addEventListener('submit', onSubmit)
   
 });
