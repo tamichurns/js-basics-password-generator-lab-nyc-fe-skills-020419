f1565bb6004b9a8294d178978df2e127be40a022
Tami Churns <tchurns@paperlesspost.com>
2019-04-02 00:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,27 @@ window.addEventListener("load", () => {
     // create a loop that replaces the number inputted, and returns a random letter each time:
     let startingString = ''
     for (let i = 0; i < howManyCharacters; i++){
-      //what do you want the loop to do? pick a random letter in this case
+      
+      //what do you want the loop to do? pick a random letter in this case.
+      
      const randomIndex = Math.floor(Math.random() * 26)
      startingString += lowerCaseLetters[randomIndex]
+     
+     // then how do you bring in the rest of the 'const's above?
+     
+     let randomIndex = Math.floor(Math.random() * lowerCaseLetters.length)
+     startingString += lowerCaseLetters[randomIndex]
+     
+     randomIndex = Math.floor(Math.random() * upperCaseLetters.length)
+     startingString += lowerCaseLetters[randomIndex]
+     
+     randomIndex = Math.floor(Math.random() * numbers.length)
+     startingString += lowerCaseLetters[randomIndex]
+     
+     randomIndex = Math.floor(Math.random() * specialCharacters.length)
+     startingString += lowerCaseLetters[randomIndex]
+     
+     
     }
     //replace your current "result" div with outputted result
     const output = document.getElementById('result')
